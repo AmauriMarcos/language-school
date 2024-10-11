4c962ec6a5e612b22d1ffba3c459f69debe9a0d8
AmauriMarcos <amaurisantos.m@gmail.com>
2024-10-12 01:39:12 +0200

    remove shadeColor func

--- src/components/ui/BlobButton.tsx
+++ src/components/ui/BlobButton.tsx
@@ -11,24 +11,6 @@ import './BlobButton.css'; // Import the CSS file
  * @param percent - The percentage to lighten or darken the color.
  * @returns The shaded HEX color code.
  */
-function shadeColor(color: string, percent: number): string {
-  const num = parseInt(color.slice(1), 16);
-  const amt = Math.round(2.55 * percent);
-  const R = (num >> 16) + amt;
-  const G = ((num >> 8) & 0x00ff) + amt;
-  const B = (num & 0x0000ff) + amt;
-  return (
-    '#' +
-    (
-      0x1000000 +
-      (R < 255 ? (R < 0 ? 0 : R) : 255) * 0x10000 +
-      (G < 255 ? (G < 0 ? 0 : G) : 255) * 0x100 +
-      (B < 255 ? (B < 0 ? 0 : B) : 255)
-    )
-      .toString(16)
-      .slice(1)
-  );
-}
 
 type Size = 'sm' | 'md' | 'lg' | 'xl';
 
